849f8af3b2445a38e2ccb91de67a124819203d33
mustafamj9 <mdmustafajaveed007@gmail>
2025-06-16 17:53:51 +0530

    upload

--- disk.ps1
+++
@@ -1,9 +0,0 @@
-Invoke-Command -ScriptBlock { 
-    $disks = @(Get-Disk | Where-Object PartitionStyle -eq "RAW"); 
-    for ($i = 0; $i -lt $disks.Count; $i++) { 
-        $disknum = $disks[$i].Number; 
-        $volume = Get-Disk $disknum | Initialize-Disk -PartitionStyle GPT -PassThru | 
-                  New-Partition -AssignDriveLetter -UseMaximumSize; 
-        Format-Volume -DriveLetter $volume.Driveletter -FileSystem ReFS -NewFileSystemLabel "Data $($disknum.ToString().PadLeft(2, "0"))" -Confirm:$false 
-    } 
-}
